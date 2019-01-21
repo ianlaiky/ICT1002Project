8fb93cbf6f4e03e8bce906b4197e3fb6006f2ec8
Ian <152772a@mymail.nyp.edu.sg>
2019-01-21 23:47:57 +0800

    i

--- main.py
+++ main.py
@@ -21,33 +21,47 @@ for i in files_in_folder:
     for oldlines in message:
         # strip whitespace
         lines = oldlines.strip()
+
         if lines != "":
             # separate to 2 per list via ":" character
             data = lines.split(":")
+
             # adding a exception for books since the book info is on each separate lines
             if str(data[0]).lower() == "Books".lower():
-                # print curated_message_for_books
+
                 curated_message_for_books_list = []
-                for index,oldClines in enumerate(curated_message_for_books):
+
+                # strip white space
+                for oldClines in curated_message_for_books:
                     curatedLines = oldClines.strip()
                     if curatedLines !="":
+
+                        # list to store list of books into the dictionary for each profile
                         curated_message_for_books_list.append(curatedLines)
-                        # print curated_message_for_books_list
+
+                # poping the first index to remove the "Books:" character, left with the actual books data
                 curated_message_for_books_list.pop(0)
-                # print curated_message_for_books_list
+
+                # storing list into Books dictionary
                 dictionary["Books"]=curated_message_for_books_list
 
+                # break and stop iteration when it reaches the books
                 break
             else:
+
+                #poping list for books so to remove items that isnt books out
                 curated_message_for_books.pop(0)
                 dictionary[data[0]] = data[1]
 
     list_of_dictionary_profiles_data.append(dictionary)
-#
+
+
+# # TO pring all user profile and its data
 # print list_of_dictionary_profiles_data
-#
+
+
 # # How to retrieve: Eg: retrieve all member names and gender
-#
+
 for i in list_of_dictionary_profiles_data:
     print "Name: "+ i["Name"]
     print "Gender: "+ i["Gender"]
