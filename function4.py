cb16d7f200d1bfa7caf4b080dc34203fdbcf4a62
wutdequack <jonathanleejuler@gmail.com>
2019-01-27 17:34:35 +0800

    Getting data dynamically for Function 2 and Function 4
    Changes:
    1. Now gets data dynamically from main function based on file path for function 2 and 4

--- function4.py
+++ function4.py
@@ -6,74 +6,9 @@ Last Modified: 23/01/2019
 Requires: FuzzyWuzzy Lib and Levenshtein Libraries
 """
 
+from main import run as getData
 from fuzzywuzzy import fuzz
 import operator
-
-sample_list = [{'Name': 'Michael Jackson', 'Gender': 'M', 'Age': '29', 'Dislikes': ['durain', 'garlic', 'swimming'],
-                'Acceptable_age_range': '18-29', 'Acceptable_country': ['Singapore', 'China'],
-                'Books': ['Mere Christianity', 'Knowing God', 'The problem of Pain', 'The God who is there',
-                          'The reason for God: belief in an age of skepticism',
-                          'Experiencing God: knowing and doing the will of God, work book'],
-                'Likes': ['hotpot', 'chicken and chops', 'chilli', 'roses', 'movies'], 'Country': 'Singapore', 'id': 0},
-               {'Name': 'Lisa Marie', 'Gender': 'F', 'Age': '22', 'Dislikes': ['garlic', 'durain', 'swimming'],
-                'Acceptable_age_range': '18-30', 'Acceptable_country': ['Singapore', 'China'],
-                'Books': ['Desiring God Meditations of a Christian Hedonist', 'God Knowing',
-                          'God Favor: Breath of Heaven', 'The God is there',
-                          'The reason for God: in an age of skepticism',
-                          'Experiencing God: knowing the will of God, work book'],
-                'Likes': ['hotpot', 'chilli', 'chicken and chops', 'roses', 'movies'], 'Country': 'Singapore', 'id': 1},
-               {'Name': 'Teresa', 'Gender': 'F', 'Age': '22', 'Dislikes': ['garlic', 'durain', 'swimming'],
-                'Acceptable_age_range': '18-30', 'Acceptable_country': ['Singapore', 'China'],
-                'Books': ['Total Truth: Liberating Christianity from its Cultural Captivity',
-                          'Reflections on the Psalms',
-                          'Intercessory Prayer: How God Can Use Your Prayers to Move Heaven Earth',
-                          "God 's Favor - Breath Of Heaven", 'Letters to Malcolm: Chiefly on Prayer'],
-                'Likes': ['hotpot', 'chilli', 'chicken and chops', 'roses', 'movies'], 'Country': 'Singapore', 'id': 2},
-               {'Name': 'Carol', 'Gender': 'F', 'Age': '23', 'Dislikes': ['football', 'hunting', 'swimming'],
-                'Acceptable_age_range': '23-50', 'Acceptable_country': ['Singapore', 'China'],
-                'Books': ['Christian reflections', 'The red tent', 'The God who is there',
-                          'God in the Dock: Essays on Theology and Ethics',
-                          'Total truth: liberating christianity from its cultural captivity', 'Redeeming love'],
-                'Likes': ['Chicken rice', 'hotpot', 'Carrot cake', 'chilli crab', 'roses', 'movies'], 'Country': 'USA',
-                'id': 3}, {'Name': 'Kevin', 'Gender': 'M', 'Age': '25', 'Dislikes': ['curry', 'garlic', 'basketball'],
-                           'Acceptable_age_range': '10-33', 'Acceptable_country': 'Singapore',
-                           'Books': ['Human Philosohy', 'Adventures in Human Being', 'Meditations on the Philosophy',
-                                     'Essay Concerning Human topic', 'Principles of Human being'],
-                           'Likes': ['fish', 'chiken', 'chocolate', 'roses', 'durain', 'movies'],
-                           'Country': 'Singapore', 'id': 4},
-               {'Name': 'Rose', 'Gender': 'F', 'Age': '25', 'Dislikes': ['chilli', 'garlic', 'basketball'],
-                'Acceptable_age_range': '18-35', 'Acceptable_country': ['Singapore', 'China', 'UK'],
-                'Books': ['Human Philosohy book', 'Adventures in Human Being', 'Towards the Philosophy',
-                          'Essay Related Human Topics', 'Human Being Shall Know Philosohy'],
-                'Likes': ['fish', 'honey', 'chocolate', 'roses', 'durain', 'running'], 'Country': 'Singapore', 'id': 5},
-               {'Name': 'Shelley', 'Gender': 'F', 'Age': '24', 'Dislikes': ['chilli', 'garlic', 'basketball'],
-                'Acceptable_age_range': '18-35', 'Acceptable_country': ['Singapore', 'China', 'USA'],
-                'Books': ['Human Philosohy book', 'Adventures in Human Being', 'Towards the Philosophy',
-                          'Essay Related Human Topics', 'Human Being Shall Know Philosohy'],
-                'Likes': ['fish', 'honey', 'chocolate', 'roses', 'durain', 'running'], 'Country': 'China', 'id': 6},
-               {'Name': 'Joel Jackson', 'Gender': 'M', 'Age': '29', 'Dislikes': ['durain', 'garlic', 'swimming'],
-                'Acceptable_age_range': '18-33', 'Acceptable_country': ['Singapore', 'China', 'USA'],
-                'Books': ['Human Philosohy', 'Nicomachean Ethics', 'Summa Theologiae',
-                          'Meditations on First Philosophy', 'Essay Concerning Human Understanding',
-                          'Principles of Human Knowledge', 'Enquiry Concerning Human Understanding', 'Social Contract',
-                          'Phenomenology of Spirit'],
-                'Likes': ['hotpot', 'chicken and chops', 'chilli', 'roses', 'movies'], 'Country': 'Singapore', 'id': 7},
-               {'Name': 'Jenny Wang', 'Gender': 'F', 'Age': '25', 'Dislikes': ['durain', 'garlic', 'swimming'],
-                'Acceptable_age_range': '23-60', 'Acceptable_country': ['China', 'USA', 'Singapore'],
-                'Books': ['Mere Christianity', 'Knowing God',
-                          'How should we then live? the rise and decline of western thought and culture',
-                          'The problem of Pain', 'The God who is there',
-                          'The reason for God: belief in an age of skepticism',
-                          'Experiencing God: knowing and doing the will of God, work book'],
-                'Likes': ['hotpot', 'chicken and chops', 'chilli', 'roses', 'movies'], 'Country': 'China', 'id': 8},
-               {'Name': 'Angela Little', 'Gender': 'F', 'Age': '22',
-                'Dislikes': ['hotpot', 'chilli', 'roses', 'movies'], 'Acceptable_age_range': '18-30',
-                'Acceptable_country': ['Singapore', 'China'],
-                'Books': ['Christian reflections', 'The red tent', 'The God who is there',
-                          'God in the Dock: Essays on Theology and Ethics',
-                          'Total truth: liberating christianity from its cultural captivity', 'Redeeming love'],
-                'Likes': ['garlic', 'durain', 'swimming', 'chicken and chops'], 'Country': 'Singapore', 'id': 9}]
-
 
 def all_books(id_list, input_list):
     """
@@ -214,6 +149,8 @@ def func4(name, input_list):
 
 
 def main():
+    # Get data dynamically based on the profiles listed from filepath
+    sample_list = getData('./profiles/')
     func4('Joel Jackson', sample_list)
 
 
