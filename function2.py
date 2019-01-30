2cead9e29d37e0d3dfba4e24da2e1db5aa24bcc5
notjyan <jun_yan24@hotmail.com>
2019-01-30 21:55:14 +0800

    Function 2 added age matching

--- function2.py
+++ function2.py
@@ -9,6 +9,7 @@ def list_matched_country(value, data_list, id_or_name=1):
 
     """
     List all the students that fall in to the acceptable country of B should be printed out
+    (After matching for opposite gender, acceptable age)
     Param:
         value -> Name (str)
         data_list -> Data input (list)
@@ -21,6 +22,7 @@ def list_matched_country(value, data_list, id_or_name=1):
     user_acceptable_country_list = []
     matched_users = []
     user_gender = ""
+    acceptable_age_list = []
     # To choose output type, 0 -> id, 1 -> Name
     if id_or_name == 0:
         output_type = "id"
@@ -43,6 +45,8 @@ def list_matched_country(value, data_list, id_or_name=1):
             # Record index
             user_index = data_list.index(user)
             user_gender = user['Gender']
+            acceptable_age_list = user['Acceptable_age_range'].split("-")
+            acceptable_age_list = map(int, acceptable_age_list)
             # Checks if the dict value is a str
             if isinstance(user['Acceptable_country'], str):
                 user_acceptable_country_str = user['Acceptable_country']
@@ -52,6 +56,10 @@ def list_matched_country(value, data_list, id_or_name=1):
                 user_acceptable_country_list = user['Acceptable_country']
                 str_or_list = 1
             break
+
+#    print acceptable_age_list
+#    print "max = ", max(acceptable_age_list)
+#    print "min = ", min(acceptable_age_list)
 
     """
     Iterates through the data_list, ignores selected user's index and compares the other user's country to
@@ -63,6 +71,7 @@ def list_matched_country(value, data_list, id_or_name=1):
         print "Error: (Function 2)Unable to retrieve gender of user"
     elif str_or_list == -1:
         print "Error: (Function 2)String or List"
+    # If User_acceptable_country is stored as a string
     elif str_or_list == 0:
         for other_user in data_list:
             # Ignore the selected user
@@ -71,9 +80,13 @@ def list_matched_country(value, data_list, id_or_name=1):
             # Ignore the same sex
             elif other_user['Gender'] == user_gender:
                 continue
+            # Match user's acceptable countries with other_user's country
             elif other_user['Country'] == user_acceptable_country_str:
-                # Appending only names for now
-                matched_users.append(other_user[output_type])
+                # If other_user's age falls in between the acceptable age range of the selected user
+                if min(acceptable_age_list) <= int(other_user['Age']) <= max(acceptable_age_list):
+                    # Appending matched other_user to the matched_users list
+                    matched_users.append(other_user[output_type])
+    # If User_acceptable_country is stored as a list
     elif str_or_list == 1:
         for country in user_acceptable_country_list:
             for other_user in data_list:
@@ -83,11 +96,13 @@ def list_matched_country(value, data_list, id_or_name=1):
                 # Ignore the same sex
                 elif other_user['Gender'] == user_gender:
                     continue
+                # Match user's acceptable countries with other_user's country
                 elif other_user['Country'] == country:
-                    # Appending only names for now
-                    matched_users.append(other_user[output_type])
+                    # If other_user's age falls in between the acceptable age range of the selected user
+                    if min(acceptable_age_list) <= int(other_user['Age']) <= max(acceptable_age_list):
+                        # Appending matched other_user to the matched_users list
+                        matched_users.append(other_user[output_type])
 
-#    print "Selected user : ", user['Name']
     return matched_users
 
 
